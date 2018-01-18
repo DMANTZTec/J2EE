58a9df08518ad3d559d00106dadbf19d8f1cfa16
Pooja <pooja.narahariseety@gmail.com>
2018-01-18 11:32:41 +0530

    new....

--- Shopping Cart/src/shoppingcart/InventoryManger.java
+++ Shopping Cart/src/shoppingcart/InventoryManger.java
@@ -1,5 +1,8 @@
 package shoppingcart;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.*;
 import java.util.*;
 
@@ -7,47 +10,141 @@ import testcases.*;
 
 public class InventoryManger {
 	
-	static Scanner s = new Scanner (System.in);
+	Scanner s = new Scanner (System.in);
 	
-	String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
-	static String DB_URL = "jdbc:mysql://localhost:3306/test";
-	static String USER = "pooja";
-	static String PASS = "pooja";
-	   
-	public static Connection conn = null;
-	public static Statement stmt = null;
 	
-		public static void main(String[] args) throws Exception {
-			  
-			Class.forName("com.mysql.jdbc.Driver");
-			System.out.println("Connecting to database...");
-		    conn = DriverManager.getConnection(DB_URL,USER,PASS);
-	   	    System.out.println("Creating statement...");
-		    stmt = conn.createStatement();
-		    int sw = 0;
-		    while(sw != 5)
-		    {
-		    	System.out.println("\n1.View Items\n2.Create Items\n3.Update Items\n4.Delete Items\n5.exit");
-			    sw = s.nextInt();
-			    switch(sw)
-			    {
-			    case 1 : ViewItems VI = new ViewItems();
-			    		 VI.test();
-			    		 break;
-			    case 2 : CreateItem CI = new CreateItem();
-			    		 CI.test();
-			    		 break;
-			    case 3 : UpdateItem UI = new UpdateItem();
-			    		 UI.test();
-			    		 break;
-			    case 4 : DeleteItem DI = new DeleteItem();
-			    		 DI.test();
-			    		 break;
-			    }
-		    }
-		    
-			stmt.close();
-		    conn.close();
-		}
+	public void CreateItems() throws Exception {
+		
+		BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\Home\\ShoppingCart\\Create_Items.txt"));
+	    String line = null;
 
+	    String II,IN,IP,IC,IS,IQ;
+	    int J = 0;
+	    while ((line = br.readLine()) != null) {  	
+	    	System.out.println(line);
+	    	J++;
+	    	StringTokenizer tokenizer = new StringTokenizer(line, ",");
+	    	
+		    	II = tokenizer.nextToken();
+		    	IN = tokenizer.nextToken();
+		    	IP = tokenizer.nextToken();
+		    	IC = tokenizer.nextToken();
+		    	IS = tokenizer.nextToken();
+		    	IQ = tokenizer.nextToken();
+		    
+	    	while (tokenizer.hasMoreTokens())
+	    	{
+	    		System.out.println(tokenizer.nextToken());
+	    		tokenizer.nextToken();
+	    	}
+		
+		try{
+			   
+			   String sql,insert;
+			   insert = "Insert into inventory (ID,Name,Price,Category,Subcategory,Quantity) values(?,?,?,?,?,?)";
+			      
+			   PreparedStatement p = testcases.InventoryMangerTest.conn.prepareStatement(insert);
+			     
+			   p.setString(1,II);
+			   p.setString(2,IN);
+			   p.setString(3,IP);
+			   p.setString(4,IC);
+			   p.setString(5,IS);
+			   p.setString(6,IQ);
+			     
+			   sql = "SELECT * FROM inventory";
+			   p.executeUpdate();
+			   ResultSet rs = testcases.InventoryMangerTest.stmt.executeQuery(sql);
+			   
+			   Item i = new Item(II,IN,IP,IC,IS,IQ);
+	    	   String JSON = JSONobject.JavaObjtoJSON(i);
+	    	   System.out.println(JSON);
+
+		      rs.close();
+		      
+		}catch(SQLException se){
+		      //Handle errors for JDBC
+		      se.printStackTrace();
+		
+		}catch(Exception e){
+		      //Handle errors for Class.forName
+		      e.printStackTrace();
+		}
+	    }
+	}
+	
+	public void DeleteItem() throws SQLException {
+		
+		System.out.println("Enter the Item ID to delete : ");  
+		String II = s.nextLine();
+		
+		String delete ;
+		delete = "DELETE FROM inventory WHERE ID = ? ";
+		PreparedStatement p = testcases.InventoryMangerTest.conn.prepareStatement(delete);
+		
+		p.setString(1,II);
+		
+		p.executeUpdate();
+	    p.close();
+		
+	}
+	
+	public void UpdateItems() throws SQLException {
+			
+	    System.out.println("Enter the Item ID : ");  
+	    String II = s.nextLine();
+		
+	    String update = null ;
+	    
+	    System.out.println("1.Price\n2.Quantity");
+	    int sw = s.nextInt();
+	    int I = 0;
+	    switch(sw)
+	    {
+	    case 1 : System.out.println("Enter the updated Item price : ");  
+				 int IP = s.nextInt();
+				 I = IP;
+				 update = "UPDATE inventory SET Price = ? WHERE ID = ? ";
+	    		 break;
+	    case 2 : System.out.println("Enter the updated Item Quantity : ");  
+				 int IQ = s.nextInt(); 
+				 I = IQ;
+				 update = "UPDATE inventory SET Quantity = ? WHERE ID = ? ";
+	    		 break;
+	    }
+		
+	    PreparedStatement p = testcases.InventoryMangerTest.conn.prepareStatement(update);
+		
+		p.setInt(1,I);
+		p.setString(2,II);
+		
+		p.executeUpdate();
+	    p.close();
+		
+	}
+	
+	public void ViewItems() {
+		
+		try
+		{
+			String view = "SELECT * FROM inventory";
+			
+			System.out.println("  ID  " +"   ITEM");
+			
+			ResultSet rs = testcases.InventoryMangerTest.stmt.executeQuery(view);
+            while (rs.next())
+            {
+                System.out.print( rs.getString(1)+ "  ");
+                System.out.println( rs.getString(2)+ "  ");
+                                
+            }
+           
+		}
+		catch(Exception e)
+		{
+			System.out.println("Exception!");
+			e.printStackTrace();
+		}
+	
+	}
 }
