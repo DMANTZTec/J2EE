66fad5f0499c2ac85576e6ec4bf2354094213946
ssreddys <ssreddys410@gmail.com>
2019-03-18 15:22:41 +0530

    Sumary of Variables

--- VariableExamples/src/com/dmantz/staticvariable/StaticVariable.java
+++ VariableExamples/src/com/dmantz/staticvariable/StaticVariable.java
@@ -5,7 +5,7 @@ public class StaticVariable {
 	/*Defination -> if the value of a variable is fixed for all objects , Such type of variables we have to declare at class level by using static keyword.
 	 
 	 Declaration -> Static variables should be declared with in the class but outside of any method or block or
-                    constructor 
+                    constructor
                     
      Scope of static variable ->Static variables will be created at the time of class loading and destroyed at the time of unloading.
 	 
@@ -14,12 +14,12 @@ public class StaticVariable {
 	
 	//For the static variables if we dont perform any initialization JVM will always provide default values
 	  static int x;     
-	//create and initialize static variable
+	//create and initialize static variable.
 	static int y=20; 
 	public static void main(String []args) {
 	StaticVariable sv=new StaticVariable();
 	System.out.println("The Value of x=" +sv.x);	
-	//Not Recemended to call StaticVariable in this way but valid
+	//Not recemended to call static variables in this way but valid 
 	System.out.println("The Value of y=" +sv.y);	
 	 //Recemended to call StaticVaribles in this way and valid
 	System.out.println("The Value of z=" +StaticVariable.y);
