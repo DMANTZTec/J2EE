d1d00b10efcff33a3714b5a4625039a404f9d385
vijay5677 <vijay.gade5677@gmail.com>
2020-02-05 12:40:01 +0530

    fourth commit

---
+++ simplemethod.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class simplemethod {
+	public static void main(String args[]) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		double x, y, z;
+		
+		x = input.nextDouble();
+		y = input.nextDouble();
+		z = x / y;
+		System.out.println("your answer is: " +z);
+		
+
+	}
+
+}
