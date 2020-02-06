e307898a4c32c14a20c8d412349930e37d5ba0e4
vijay5677 <vijay.gade5677@gmail.com>
2020-02-06 14:10:55 +0530

    example program of if and else condition

---
+++ passfail.java
@@ -0,0 +1,15 @@
+class passfail{
+	public static void main(String args[]){
+	
+	int maths, physics, english;
+	maths = 75;
+	physics = 36;
+	english = 90;
+	if(physics > 35 && maths > 35 && english > 35) {
+		System.out.println("pass");
+	}else {
+		System.out.println("fail");
+			}
+		}
+	
+} 
