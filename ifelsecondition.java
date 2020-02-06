ee3c8c5b67426b46f65d89e6b076409178471c59
vijay5677 <vijay.gade5677@gmail.com>
2020-02-06 12:19:06 +0530

    sample program on if-else condition

---
+++ ifelsecondition.java
@@ -0,0 +1,15 @@
+
+public class ifelsecondition {
+
+	public static void main(String[] args) {
+		int apples, mangos;
+		apples = 10;
+		mangos = 10;
+		if(apples == mangos) {
+			System.out.println("yes!");
+		}else {
+			System.out.println("no!");
+		}
+	}
+
+}
