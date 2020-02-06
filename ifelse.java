5c181518c02a51db76acf4785dfcae90ad184a86
vijay5677 <vijay.gade5677@gmail.com>
2020-02-06 15:31:04 +0530

    example of if and else condition

---
+++ ifelse.java
@@ -0,0 +1,33 @@
+class passfail{
+	public static void main(String args[]){
+		int maths, physics, hindi, english, telugu;
+		
+	maths = 80;
+	physics =95;
+	hindi = 86;
+	english = 90;
+	telugu = 95;
+	
+	if(maths > 75) {
+		System.out.println("pass");
+	}else {
+		System.out.println("fail");
+	}if(physics > 90) {
+		System.out.println("pass");
+	}else {
+		System.out.println("fail");
+	}if(hindi > 88) {
+		System.out.println("pass");
+	}else {
+		System.out.println("fail");
+	}if(english > 85) {
+		System.out.println("pass");
+	}else {
+		System.out.println("fail");
+	}if(telugu > 90) {
+		System.out.println("pass");
+	}else {
+		System.out.println("fail");
+	}
+	}
+}
