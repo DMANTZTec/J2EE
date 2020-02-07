99173076f07ffae69faea3b7a7782990e290377f
vijay5677 <vijay.gade5677@gmail.com>
2020-02-07 15:14:45 +0530

    example of if and else statement

--- passfail.java
+++ passfail.java
@@ -1,15 +1,26 @@
-class passfail{
-	public static void main(String args[]){
-	
-	int maths, physics, english;
-	maths = 75;
-	physics = 36;
-	english = 90;
-	if(physics > 35 && maths > 35 && english > 35) {
-		System.out.println("pass");
-	}else {
-		System.out.println("fail");
-			}
+class passfail {
+	public static void main(String args[]) {
+		int english, hindi, telugu, sanskrit;
+		english = 100;
+		hindi = 100;
+		telugu = 100;
+		sanskrit = 100;
+		if(english >= 80) {
+			System.out.println("english  pass");
+		}else {
+			System.out.println("english  fail");
+		}if(hindi >= 101) {
+			System.out.println("hindi  pass");
+		}else {
+			System.out.println("hindi  fail");
+		}if(telugu >= 80) {
+			System.out.println("telugu  pass");
+		}else {
+			System.out.println("telugu  fail");
+		}if(sanskrit >= 99) {
+			System.out.println("sanskrit  pass");
+		}else {
+			System.out.println("sanskrit  fail");
 		}
-	
-} 
+	}
+}
