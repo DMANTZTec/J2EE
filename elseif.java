843f29fc1f9c77fe70bce831a076c35ef6e7e991
vijay5677 <vijay.gade5677@gmail.com>
2020-02-11 14:53:23 +0530

    elseif condition example

---
+++ elseif.java
@@ -0,0 +1,28 @@
+class elseif {
+  public static void main(String agrs[]) {
+	  int eng, mat, scn, tel;
+	  eng = 50;
+	  mat = 45;
+	  scn = 48;
+	  tel = 50;
+	  if(eng >= 19) {
+		  System.out.println("english 50 pass");
+	  }else {
+		  System.out.println("fail");
+	  }if(mat >= 19) {
+		  System.out.println("mat 45 pass");
+	  }else {
+		  System.out.println("fail");
+	  }if(scn >= 19) {
+		  System.out.println("scn 48 pass");
+	  }else {
+		  System.out.println("fail");
+	  }if(tel >= 19) {
+		  System.out.println("tel 50 pass");
+	  }else {
+		  System.out.println("fail");
+	  }
+	  
+  }
+  
+}
