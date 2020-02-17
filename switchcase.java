d898af0dec2272ef7adfaa32a5364b794b92e0a1
vijay5677 <vijay.gade5677@gmail.com>
2020-02-17 17:11:37 +0530

    switch statement

---
+++ switchcase.java
@@ -0,0 +1,24 @@
+class switchcase {
+	public static void main(String args[]) {
+		int x = 3;
+		switch(x) {
+		
+		case 0:
+			System.out.println("x=1");
+			break;
+			
+		case 1:
+			System.out.println("x=2");
+			break;
+			
+		case 3:
+			System.out.println("x=3");
+			break;
+			
+		case 4:
+			System.out.println("x=4");
+			break;
+		}
+		
+	}
+}
