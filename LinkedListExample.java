96705eb61b29d5821618e28d4fa30df805032471
vijayreddy <59818512+vijay5677@users.noreply.github.com>
2020-03-05 16:35:49 +0530

    LinkedList Example

---
+++ LinkedListExample.java
@@ -0,0 +1,22 @@
+package com.example.javaInterface;
+
+import java.util.LinkedList;
+
+import javax.swing.text.html.HTMLDocument.Iterator;
+
+public class LinkedListExample {
+	public static void main(String args[]){  
+		LinkedList<String> al=new LinkedList<String>();  
+//		al.add("Ravi");  
+//		al.add("Vijay");  
+//		al.add("Ravi");  
+//		al.add("Ajay");  
+		
+		//System.out.println(al.get(3));
+		//al.pop();
+		System.out.println(al);
+		}  
+		}  
+		
+
+
