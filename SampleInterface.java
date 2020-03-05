4d3c4228138d946cbe8bfca2b80821c4d0346994
vijayreddy <59818512+vijay5677@users.noreply.github.com>
2020-03-05 16:37:38 +0530

    Sample Interface Example

---
+++ SampleInterface.java
@@ -0,0 +1,24 @@
+package com.example.javaInterface;
+
+public interface SampleInterface {
+	int constatnt_value = 5;
+
+	void method1toimplement();
+	void methos2toimplement();
+}
+
+class SampleImplementation implements SampleInterface {
+
+	@Override
+	public void method1toimplement() {
+		//Body
+		
+	}
+
+	@Override
+	public void methos2toimplement() {
+		//Body
+		
+	}
+	
+}
