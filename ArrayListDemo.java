acfa64534b07257f14c84ef520894b14e3a2e6b3
vijayreddy <59818512+vijay5677@users.noreply.github.com>
2020-03-06 11:55:27 +0530

    Arraylist demo example

---
+++ ArrayListDemo.java
@@ -0,0 +1,21 @@
+package com.example.javaInterface;
+
+import java.util.ArrayList;
+
+class ArrayListDemo 
+{
+	public static void main(String args[])
+	{
+		ArrayList<String> list = new ArrayList();
+		
+		list.add("123");
+		list.add("vijay");
+		list.add("Apple");
+		list.add("Banana");
+		list.add("All set");
+		
+		System.out.println("list");
+	}
+	
+
+}
